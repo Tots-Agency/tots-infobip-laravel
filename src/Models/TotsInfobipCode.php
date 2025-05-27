b75f68cc3a0d8b5d7f8d7e4e661f5089c6cfcdad
Matias Camiletti <matias.camiletti@gmail.com>
2025-05-27 08:03:50 -0300

    Add TotsInfobipCode model extending TotsUserCode

---
+++ src/Models/TotsInfobipCode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tots\Infobip\Models;
+
+use Tots\AuthTfaBasic\Models\TotsUserCode;
+
+/**
+ * Description of Model
+ *
+ * @author matiascamiletti
+ */
+class TotsInfobipCode extends TotsUserCode
+{
+    const PROVIDER_INFOBIP = 2;
+}
